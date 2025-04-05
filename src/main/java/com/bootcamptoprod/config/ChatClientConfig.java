5f759c0516e0f8d84deff02d6f807d392b6ee65d
Bunty Raghani <bootcamptoprod@gmail.com>
2025-04-06 00:44:54 +0530

    Adding Spring AI logbook logging implementation

--- src/main/java/com/bootcamptoprod/config/ChatClientConfig.java
+++ src/main/java/com/bootcamptoprod/config/ChatClientConfig.java
@@ -1,8 +1,11 @@
 package com.bootcamptoprod.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.boot.web.client.RestClientCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.zalando.logbook.Logbook;
+import org.zalando.logbook.spring.LogbookClientHttpRequestInterceptor;
 
 @Configuration
 public class ChatClientConfig {
@@ -10,6 +13,11 @@ public class ChatClientConfig {
     @Bean
     public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
         return chatClientBuilder.build();
+    }
+
+    @Bean
+    public RestClientCustomizer restClientCustomizer(Logbook logbook) {
+        return restClientBuilder -> restClientBuilder.requestInterceptor(new LogbookClientHttpRequestInterceptor(logbook));
     }
 
 }
--- src/main/java/com/bootcamptoprod/controller/ChatController.java
+++ src/main/java/com/bootcamptoprod/controller/ChatController.java
@@ -2,7 +2,6 @@ package com.bootcamptoprod.controller;
 
 
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +20,6 @@ public class ChatController {
     @PostMapping("/ask")
     public String chat(@RequestBody String userInput) {
         return chatClient.prompt(userInput)
-                .advisors(new SimpleLoggerAdvisor())
                 .call().content();
     }
 }
